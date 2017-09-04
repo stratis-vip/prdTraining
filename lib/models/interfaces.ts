0af616b490ebb58b41c1e84265fe929069ad2d44
Stratis Christodoulou <stratis.vip@gmail.com>
2017-09-04 14:53:19 +0300

    εργασία Δευτέρας πρωί

--- lib/models/interfaces.ts
+++ lib/models/interfaces.ts
@@ -4,8 +4,29 @@ export enum actions {
     ACTION_REQUIRED
 }
 
+
 //χρειάζεται για να έχω στάνταρ τρόπο απάντησης από τα promises
-export interface answer{
+interface IAnswer{
     reason: string;
     action: actions;
+    errId:number;
 }
+
+export class answer implements IAnswer{
+    static errid = 0;
+    reason: string;
+    action: actions;
+    errId: number; 
+    constructor(reason:string,action:actions)
+    {
+        this.reason = reason;
+        this.action = action;
+        this.errId= answer.getNumber();
+    }
+
+    static getNumber(){
+        return ++answer.errid;
+    }
+}
+
+
--- lib/models/models.ts
+++ lib/models/models.ts
@@ -6,7 +6,6 @@ let errors: Array<answer> = [];
 let steps: Array<answer> = [];
 
 
-
 //check if there is a table name athletes
 //let checkIfTableExists = 
 
@@ -15,20 +14,37 @@ let checkIfTableExists = (tableName: string): Promise<answer> => {
     return new Promise((resolve, reject) => {
         let query: string = `SELECT name FROM sqlite_master WHERE type='table' AND name='${tableName}'`;
         db.get(query, (err: Error | null, row: any) => {
-            if (err) { reject({ reason: err.message, action: actions.STOP_PROGRAM_ERROR }); } // κάποιο λάθος παρουσιάστηκε
+            if (err) { reject(new answer(err.message, actions.STOP_PROGRAM_ERROR )); } // κάποιο λάθος παρουσιάστηκε
             else {
                 if (row === undefined) {
-                    reject({ reason: "Table not exists", action: actions.ACTION_REQUIRED });
+                    reject(new answer(`Table ${tableName} not exists`, actions.ACTION_REQUIRED ));
                 } // ο πίνακας δεν υπάρχει
                 else {
                     if (row.name === tableName) {
-                        resolve({ reason: "Table Exists", action: actions.NO_ACTION });
+                        resolve(new answer("Table Exists", actions.NO_ACTION));
                     }
-                    else { reject({ reason: "Table not exists", action: actions.ACTION_REQUIRED }) }
+                    else { reject(new answer(`Table ${tableName} not exists`, actions.ACTION_REQUIRED )) }
                 }
             }
         });
     });
+}
+
+let createTableAthletes = ():Promise<answer> =>{
+    return new Promise(()=>{
+        db.get(`CREATE TABLE 'athletes' (
+            'id'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            'name'	TEXT NOT NULL DEFAULT 'Ανώνυμος  Αθλητής',
+            'weight'	REAL NOT NULL DEFAULT 75.0,
+            'height'	REAL NOT NULL DEFAULT 1.73,
+            'sex'	INTEGER NOT NULL DEFAULT 0,
+            'bday'	TEXT NOT NULL,
+            'vo2max'	TEXT NOT NULL`,(err:Error | null, row:any) {
+                //check for error and reject
+                //resolve if everything all right
+            }
+        );
+    })
 }
 
 
@@ -37,9 +53,10 @@ let db = new sql.Database('training', sql.OPEN_READWRITE | sql.OPEN_CREATE);
 checkIfTableExists('athletes')
     .then((value) => {
         steps.push(value);
+        console.log(`STEPS\n${JSON.stringify(steps,null,2)}`);
     })
     .catch((value) => {
-        console.log(`table athletes does not exists ${JSON.stringify(value)}`);
+        errors.push(value);
         switch (value.action) {
             case actions.ACTION_REQUIRED:
                 console.log(`TODO -> create Table Athletes`)
@@ -52,23 +69,5 @@ checkIfTableExists('athletes')
                 break;
             case actions.NO_ACTION:
         }
+        console.log(`ERROS\n${JSON.stringify(errors,null,2)}`);
     });
-
-    
-   
-
-/*
-db.get('SELECT * FROM ATHLETES',(err:Error | null, row:any,)=>
-{
-    if (err){
-        return console.log(err);
-    }else{
-        if (row){
-        console.log(row);}
-        else
-            {
-                console.log('no row');
-            }
-    }
-}
-)*/

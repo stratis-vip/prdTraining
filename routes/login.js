6b7f74f5657f881d08273978f501bca93cefc37b
Στρατής Χριστοδούλου <stratis.vip@gmail.com>
2017-09-16 23:00:13 +0300

    delete .js files

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-exports.router = router;
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-    res.render('login', { title: 'Είσοδος στον βοηθό Προπονητή' });
-});
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-exports.router = router;
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-    res.send('respond with a resource');
-});

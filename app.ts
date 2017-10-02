ff99a3f420cccc625669950ddb3a0dfe407721e0
Στρατής Χριστοδούλου <stratis.vip@gmail.com>
2017-10-02 22:58:36 +0300

    init

--- app.ts
+++ app.ts
@@ -39,6 +39,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(session(options));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'views/public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
